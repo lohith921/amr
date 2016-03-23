cb327d5e065ab1876a0a811ec7571b0507fcfeae
Lohith <nanuvala.lohith@gmail.com>
2016-03-22 22:54:22 -0500

    touble with creating a neighbor map, modify map_create and map_set to match according to neighbor file information

--- sample1.c
+++ sample1.c
@@ -1,4 +1,4 @@
-/* This program takes input a .ele file or a .node files with switches -e and -n resp*/
+/* This program takes input a .ele file or a .node files and a .neigh file with switches -e, -n and -p resp*/
 /* There is a small bug, the name of the file without extension should be 3 or more characters length*/
 #ifdef SINGLE
 #define REAL float
@@ -17,86 +17,90 @@
 #define INPUTLINESIZE 1024
 
 /* Structure to hold properties */
-struct amrgeo {
-int nodeindi;//nodefile indicator
-int eleindi;//element file indicator
-char elefilename[FILENAMESIZE];
-char nodefilename[FILENAMESIZE];
-REAL minlength;// min length could be passed from command line
-};
-static int num_ele=0;// to keep track of total no of elements
-static int num_nodes=0;// to keep track of total no of nodes
+ struct amrgeo {
+       int nodeindi; // nodefile indicator
+       int eleindi; // element file indicator
+       int neighbourindi;// neighbor file indicator
+       char elefilename[FILENAMESIZE];
+       char nodefilename[FILENAMESIZE];
+       char nieghborfilename[FILENAMESIZE];
+       REAL minlength;// min length could be passed from command line
+       };
+
+ static int num_ele=0;// to keep track of total no of elements
+ static int num_nodes=0;// to keep track of total no of nodes
 /* structure for elements*/
-struct element{
-	int ele_no;
-	int nodes[3];
-	struct element *next;
-} *head_ele; 
+ struct element{
+	   int ele_no;
+	   int nodes[3];
+       struct element *next;
+       } *head_ele; 
 
 /* structure for nodes*/
-struct node{
-	int node_no;
-	REAL vertices[2];
-	struct node *next;
-} *head_node;
+ struct node{
+       int node_no;
+       REAL vertices[2];
+	   struct node *next;
+       } *head_node;
 
 /*structure to hold vertices */
-struct vertex{
- int no;
- REAL *values;
-}; 
+ struct vertex{
+       int no;
+       REAL *values;
+       }; 
 
 /*********************************************************************/ 
-void info(){
-printf("This program is used to refine the mesh using edge bisection.");
-printf("This program uses an .ele file and a .node file.");
-printf("Usage shall be\n");
-printf("amrgeo -en <filename>\n");
-}
+ void info(){
+      printf("This program is used to refine the mesh using edge bisection.");
+      printf("This program uses an .ele, a .node and a .neigh file.");
+      printf("Usage shall be\n");
+      printf("amrgeo -enp <filename>\n");
+      }
 /*********************************************************************/
-void syntax(char *pname){
-  printf("%s [-en] input_file\n",pname);
-  printf("    -e  element file indication.\n");
-  printf("    -n  node file indication\n");
-  exit(0);
-}
+ void syntax(char *pname){
+      printf("%s [-enp] input_file\n",pname);
+      printf("    -e  element file indication.\n");
+      printf("    -n  node file indication\n");
+      printf("    -p  neighbour file indication\n");
+      exit(0);
+      }
 /*********************************************************************/
 // following function is used to read a line from the file
-char *readline(char *string, FILE *infile, char *infilename){
-  char *result;
-  do {
-    result = fgets(string, INPUTLINESIZE, infile);
-    if (result == (char *) NULL) {
-      printf("Error:  Unexpected end of file in %s.\n",
+ char *readline(char *string, FILE *infile, char *infilename){
+      char *result;
+      do {
+         result = fgets(string, INPUTLINESIZE, infile);
+         if (result == (char *) NULL) {
+            printf("Error:  Unexpected end of file in %s.\n",
              infilename);
-      exit(1);
-    }
-    while ((*result != '\0') && (*result != '#')
+            exit(1);
+            }
+      while ((*result != '\0') && (*result != '#')
            && (*result != '.') && (*result != '+') && (*result != '-')
            && ((*result < '0') || (*result > '9'))) {
-      result++;
-    }
-  } while ((*result == '#') || (*result == '\0'));
-  return result;
-}
+           result++;
+           }
+      } while ((*result == '#') || (*result == '\0'));
+      return result;
+      }
 /*******************************************************************/
-char *findfield(char *string){
-  char *result;
-  result = string;
-  while ((*result != '\0') && (*result != '#')
+ char *findfield(char *string){
+      char *result;
+      result = string;
+      while ((*result != '\0') && (*result != '#')
          && (*result != ' ') && (*result != '\t')) {
-    result++;
-  }
-  while ((*result != '\0') && (*result != '#')
+            result++;
+      }
+      while ((*result != '\0') && (*result != '#')
          && (*result != '.') && (*result != '+') && (*result != '-')
          && ((*result < '0') || (*result > '9'))) {
-    result++;
-  }
-  if (*result == '#') {
-    *result = '\0';
-  }
-  return result;
-}
+            result++;
+      }
+      if (*result == '#') {
+         *result = '\0';
+         }
+      return result;
+      }
 /*********************************************************************/
 void display_elements(struct element *t){
 	struct element *temp;
@@ -173,8 +177,7 @@ void process(struct element *el,struct map_t *node_map, int tol){
   vertexB=(REAL *)malloc(2*sizeof(REAL));
   vertexC=(REAL *)malloc(2*sizeof(REAL));
   while(temp1!=NULL){
-  
-  if(node_map!=NULL){
+   if(node_map!=NULL){
    vertexA=map_get(node_map,temp1->nodes[0]);
    vertexB=map_get(node_map,temp1->nodes[1]);
    vertexC=map_get(node_map,temp1->nodes[2]);
@@ -186,41 +189,43 @@ void process(struct element *el,struct map_t *node_map, int tol){
   //length calculation begins
   //printf("calling calc_length AB for element %d\n",temp1->ele_no);
   struct element *new;
-  length=calc_length(vertexA,vertexB);
-   if(length>tol){
+  length = calc_length(vertexA,vertexB);
+   if(length >= tol){
     // printf("89\n");
-    new=(struct element *)malloc(sizeof(struct element));
-    new->next=NULL;
-    new->ele_no=0;
-    for(i=0;i<3;i++){
-      new->nodes[i]=0;
+    new = (struct element *)malloc(sizeof(struct element));
+    new->next = NULL;
+    new->ele_no = 0;
+    for(i = 0;i < 3;i++){
+      new->nodes[i] = 0;
     }
-    midAB=(REAL *)malloc(2*sizeof(REAL));
-    midAB=compute_mid(vertexA,vertexB);
+    midAB = (REAL *)malloc(2*sizeof(REAL));
+    midAB = compute_mid(vertexA,vertexB);
     num_nodes++;
     map_set(node_map,num_nodes,midAB);
-    new->ele_no=++num_ele;
-    new->nodes[0]=num_nodes;
-    new->nodes[1]=temp1->nodes[1];
-    new->nodes[2]=temp1->nodes[2];
-    temp1->nodes[1]=num_nodes;
-    new->next=temp1->next;
-    temp1->next=new;//new triangles A,mid,C and mid,B,C are formed
+    new->ele_no = ++num_ele;
+    new->nodes[0] = num_nodes;
+    new->nodes[1] = temp1->nodes[1];
+    new->nodes[2] = temp1->nodes[2];
+    temp1->nodes[1] = num_nodes;
+    new->next = temp1->next;
+    temp1->next = new;//new triangles A,mid,C and mid,B,C are formed
     printf("inserted new element %d\n",new->ele_no);
-    temp1=temp1->next;
+    // We have inserted a node, but we have to check for conformity also
+    
+    temp1 = new;
    }
   //printf("calling calc_length BC for element %d \n",temp1->ele_no);
-  length=calc_length(vertexB,vertexC);
-  if(length>tol){
+  length = calc_length(vertexB,vertexC);
+  if(length > tol){
   // printf("90\n");
-   midBC=(REAL *)malloc(2*sizeof(REAL));
-   new=(struct element *)malloc(sizeof(struct element));
-   new->next=NULL;
-   new->ele_no=0;
-   for(i=0;i<3;i++){
-     new->nodes[i]=0;
+   midBC = (REAL *)malloc(2*sizeof(REAL));
+   new = (struct element *)malloc(sizeof(struct element));
+   new->next = NULL;
+   new->ele_no = 0;
+   for(i = 0;i < 3;i++){
+     new->nodes[i] = 0;
    }
-   midBC=compute_mid(vertexB,vertexC);
+   midBC = compute_mid(vertexB,vertexC);
    num_nodes++;
    map_set(node_map,num_nodes,midBC);
    new->ele_no=++num_ele;
@@ -363,11 +368,13 @@ int main(int argc, char **argv)
  filename[0]='\0';
 // printf("0.25\n");
  REAL vertices[2];
+ int nbs[3];
  m=(struct amrgeo *)malloc(sizeof(struct amrgeo));
  m->nodefilename[0]='\0';
  m->elefilename[0]='\0';
- printf("0.35\n");
- FILE *fnode, *fele;
+ m->neighborfilename[0]='\0';
+ //printf("0.35\n");
+ FILE *fnode, *fele, *fneigh;
  
  head_ele=(struct element *)malloc(sizeof(struct element));
  head_node=(struct node *)malloc(sizeof(struct node));
@@ -389,6 +396,9 @@ int main(int argc, char **argv)
         else if (argv[i][j] == 'n') {
           m->nodeindi = 1;
 	 	}
+	 	else if (argv[i][j] == 'p') {
+          m->neighborindi = 1;
+	 	}
         else {
           info();
 	 	}
@@ -403,14 +413,16 @@ int main(int argc, char **argv)
  }
  
  //printf("&filename[strlen(filename) - 5] is %s\n",&filename[strlen(filename) - 5]);
- if(!strcmp(&filename[strlen(filename) - 5], ".node")) {
-   //printf("2\n");
+ if(!strcmp(&filename[strlen(filename) - 5], ".node")) { //printf("2\n"); 
    strcpy(m->nodefilename,filename);
-   memcpy(m->elefilename,&filename,(strlen(filename)-5));
- //  strcpy(root,m->elefilename);  
+   memcpy(m->elefilename,&filename,(strlen(filename)-5)); //  strcpy(root,m->elefilename);  
    strcat(m->elefilename,".ele");
    m->elefilename[strlen(m->elefilename)+1]='\0';
+   memcpy(m->neighborfilename, &filename, (strlen(filename)-5));
+   strcat(m->neighborfilename,".neigh");
+   m->neighborfilename[strlen(m->neighborfilename)+1]='\0';
    filename[strlen(filename) - 5] = '\0';
+   
  }
  else if(!strcmp(&filename[strlen(filename) - 4], ".ele")) {
   // printf("2.5\n");
@@ -419,16 +431,31 @@ int main(int argc, char **argv)
    //strcpy(root,m->nodefilename);
    strcat(m->nodefilename,".node");  
    m->nodefilename[strlen(m->nodefilename)+1]='\0';
+   memcpy(m->neighborfilename, &filename,(strlen((filename)-4));
+   strcat(m->neighborfilename,".neigh");
+   m->neighborfilename[strlen(m->neighbourfilename)+1] = '\0';
+   filename[strlen(filename) - 4] = '\0';
+ }
+ else if(!strcmp(&filename[strlen(filename) - 6], ".neigh")) {
+  // printf("2.5\n");
+   strcpy(m->neighborfilename,filename);
+   memcpy(m->nodefilename,&filename,(strlen(filename)-6));
+   //strcpy(root,m->nodefilename);
+   strcat(m->nodefilename,".node");  
+   m->nodefilename[strlen(m->nodefilename)+1]='\0';
+   memcpy(m->elefilename, &filename,(strlen((filename)-6));
+   strcat(m->elefilename,".neigh");
+   m->elefilename[strlen(m->nelefilename)+1] = '\0';
    filename[strlen(filename) - 4] = '\0';
  }
  else{
  	info();
  }
-// printf("The file names are: %s and %s\n", m->elefilename, m->nodefilename);
- root=(char *)malloc(sizeof(filename));
- root[0]='\0';
+ printf("The file names are: %s and %s\n", m->elefilename, m->nodefilename.m->neighborfile);
+ root = (char *)malloc(sizeof(filename));
+ root[0] = '\0';
  strcpy(root,filename);
- fele=fopen(m->elefilename,"r");
+ fele = fopen(m->elefilename,"r");
  //while(fnode!=NULL){
  /*do{
  	printf("%s\n",data);
@@ -436,22 +463,22 @@ int main(int argc, char **argv)
  }while(fgets(data,200,fnode)!=NULL);*/
  
  //following code is to read .ele file and create linked list of elements
- firstline=readline(data,fele,m->elefilename);
- nele=atoi(strtok(firstline,delim)); 
+ firstline = readline(data,fele,m->elefilename);
+ nele = atoi(strtok(firstline,delim)); 
 // printf("2.75\n");
- for(i=1;i<=nele;i++){
+ for(i = 1;i <= nele;i++){
  	struct element *temp;
- 	temp=(struct element *)malloc(sizeof(struct element));
- 	temp->next=NULL;
-  	firstline=readline(data,fele,m->elefilename);
+ 	temp = (struct element *)malloc(sizeof(struct element));
+ 	temp->next = NULL;
+  	firstline = readline(data,fele,m->elefilename);
        // printf("first line is %s\n",firstline);
- 	token=strtok(firstline,delim);
- 	temp->ele_no=atoi(token);
+ 	token = strtok(firstline,delim);
+ 	temp->ele_no = atoi(token);
  	j=0;
- 	while(token!=NULL){
- 		token=strtok(NULL,delim);
-		if(j!=3)
- 		 temp->nodes[j++]=atoi(token);
+ 	while(token != NULL){
+ 		token = strtok(NULL,delim);
+		if(j != 3)
+ 		 temp->nodes[j++] = atoi(token);
  	 }
 	 insert_element(temp); 	
 //printf("insert_element\n");
@@ -462,23 +489,23 @@ int main(int argc, char **argv)
  fnode=fopen(m->nodefilename,"r");
 // printf("3\n");
  // creating a map of nodes
- nodes=map_create();
+ nodes = map_create();
 
- firstline=readline(data,fnode,m->nodefilename);
- nnode=atoi(strtok(firstline,delim)); 
+ firstline = readline(data,fnode,m->nodefilename);
+ nnode = atoi(strtok(firstline,delim)); 
  //printf("no of nodes are %d\n",nnode);
- num_nodes=nnode;
- for(i=1;i<=nnode;i++){
- 	firstline=readline(data,fnode,m->nodefilename);
- 	token=strtok(firstline,delim);
+ num_nodes = nnode;
+ for(i = 1;i <= nnode;i++){
+ 	firstline = readline(data,fnode,m->nodefilename);
+ 	token = strtok(firstline,delim);
        // printf("token extracted is %s\n",token);
- 	node_no=atoi(token);
- 	j=0;
- 	while(token!=NULL){
- 		token=strtok(NULL,delim);
-	        if(token!=NULL){
+ 	node_no = atoi(token);
+ 	j = 0;
+ 	while(token != NULL){
+ 		token = strtok(NULL,delim);
+	        if(token != NULL){
                  // printf("token value again is %s\n",token);
- 		  vertices[j++]=atof(token);
+ 		  vertices[j++] = atof(token);
 		  //printf("vertices of j-1 is %f\n",vertices[j-1]);
 		}
  	 }
@@ -486,8 +513,34 @@ int main(int argc, char **argv)
  map_set(nodes,node_no,vertices);
  }
  //display_map(nodes);
+ 
+ //* Write code for reading neigh file and create a neighbor map *//
+ fneigh=fopen(m->neighborfilename,"r");
+ // creating a map of neighbors
+ neighbs = map_create();
+ firstline = readline(data,fnode,m->neighborfilename);
+ nele = atoi(strtok(firstline,delim)); 
+ //printf("no of nodes are %d\n",nnode);
+ //num_eles = nele;
+ for(i = 1;i <= nele;i++){
+ 	firstline = readline(data,fnode,m->neighborfilename);
+ 	token = strtok(firstline,delim);
+       // printf("token extracted is %s\n",token);
+ 	node_no = atoi(token);
+ 	j = 0;
+ 	while(token != NULL){
+ 		token = strtok(NULL,delim);
+	        if(token != NULL){
+               nbs[j++] = atof(token);
+            }
+ 	 }
+    map_set(neighbs,node_no,nbs);
+ }
+ 
+ 
+ 
  // printf("going to call process method\n");
- process(head_ele,nodes,0.9);
+ process(head_ele,nodes,neihbs,0.9);
  printf("processing finished no of elements are: %d no of nodes are %d\n",num_ele,num_nodes);
  printf("Displaying elements after processing\n");
  display_elements(head_ele);
@@ -497,7 +550,7 @@ int main(int argc, char **argv)
  fclose(fnode);
  fclose(fele);
  printf("files successfully opened\n");
- head_ele=sort_list(head_ele);
+ head_ele = sort_list(head_ele);
  printf("done the sorting:\n");
  //display_elements(head_ele);
  write_elements(root,head_ele);
