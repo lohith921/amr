5d57e06c0bba4300dd01ecafaf012df56b71da6d
lohith921 <nanuvala.lohith@gmail.com>
2016-03-30 12:36:48 -0500

    removed the neighbor information and updated with edge map information

--- mmap_lib.h
+++ mmap_lib.h
@@ -1,34 +1,9 @@
 /*
-{// map_lib
-// A simple associative-array library for C
-//
-// License: MIT / X11
-// Copyright (c) 2009 by James K. Lawless
-// jimbo@radiks.net http://www.radiks.net/~jimbo
-// http://www.mailsend-online.com
-//
-// Permission is hereby granted, free of charge, to any person
-// obtaining a copy of this software and associated documentation
-// files (the "Software"), to deal in the Software without
-// restriction, including without limitation the rights to use,
-// copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following
-// conditions:
-//
-// The above copyright notice and this permission notice shall be
-// included in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-// OTHER DEALINGS IN THE SOFTWARE.
-}
+ map_lib
+ A simple associative-array library for C
 */
+
+#include<vector>
 
 #ifdef SINGLE
 #define REAL float
@@ -45,19 +20,30 @@ struct map_node {
    REAL *xandy;
 };
 
-struct map_neighbor{
+struct edge_map{
+	int key1, key2;
+	std::vector<int> node_nums;
+	struct edge_map *nxt;
+};
+
+// The following are related to node map.
+struct map_node *create_nodemap();
+void map_freenode(struct map_node *map);
+void map_setnode(struct map_node *m,int pt,REAL coords[]);
+REAL* map_getnode(struct map_node *m,int pt);
+
+// The following are related to edge map.
+struct edge_map* create_edgemap();
+void free_edgemap(struct edge_map *map);
+void set_edgemap(struct edge_map *m,int k1, int k2,int node);
+void display_edgemap(struct edge_map *first);
+std::vector<int> edgemap_getnodes(struct edge_map *,int , int );
+
+#endif
+
+/*struct map_neighbor{
 	struct map_neighbor *nxt;
 	int t_num;
 	int* nbs;
 	};
-
-struct map_node *create_nodemap();
-struct map_neighbor *create_neighbormap();
-void map_freenode(struct map_node *map);
-void map_freeneighbor(struct map_neighbor *map);
-void map_setnode(struct map_node *m,int pt,REAL coords[]);
-void map_setneighbor(struct map_neighbor *m, int t, int neighbs[]);
-REAL* map_getnode(struct map_node *m,int pt);
-int* map_getneighbors(struct map_neighbor *m,int tnum);
-
-#endif
+*/
