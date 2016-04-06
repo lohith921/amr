9a68242d07bf225c0412498a5353af688d88988d
lohith921 <nanuvala.lohith@gmail.com>
2016-04-05 21:45:25 -0500

    All the triangle related structures are defined here

---
+++ structures.h
@@ -0,0 +1,45 @@
+/* This is used to store all structure definitions */
+
+#ifndef STRUCTURES_H
+#define STRUCTURES_H
+
+#define FILENAMESIZE 2048
+#define INPUTLINESIZE 1024
+
+/* Structure to hold properties */
+struct amrgeo {
+       int nodeindi; // nodefile indicator
+       int eleindi; // element file indicator
+       char elefilename[FILENAMESIZE];
+       char nodefilename[FILENAMESIZE];
+       REAL minlength;// min length could be passed from command line
+};
+
+/* structure for elements*/
+ struct element{
+	   int ele_no;
+	   int nodes[3];
+       struct element *next;
+       element(){
+       		ele_no = 0;
+       		nodes[0] = 0;
+       		nodes[1] = 0;
+       		nodes[2] = 0;
+       		next = NULL;
+       }
+} *head_ele; 
+
+/* structure for nodes*/
+ struct node{
+       int node_no;
+       REAL vertices[2];
+	   struct node *next;
+ } *head_node;
+ 
+ /*structure to hold vertices */
+ struct vertex{
+       int no;
+       REAL *values;
+ };
+ 
+ #endif
